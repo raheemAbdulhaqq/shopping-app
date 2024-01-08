0ed9c5b18b285ac2f8730f67d3160fca02e1dcd3
raheemAbdulhaqq <raheemabdulhaqq2020@gmail.com>
2024-01-08 08:51:27 +0100

    connect to mongo db and added .env

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
 const app = express()
 const port = 3000
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("mongodb connected")
+})
 
 app.listen(port, () => {
     console.log("express server is running on 3000")
