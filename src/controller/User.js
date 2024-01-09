f42e4d5fab48cf15698bd3d8bb7b09dce9bd8e18
raheemAbdulhaqq <raheemabdulhaqq2020@gmail.com>
2024-01-09 11:36:23 +0100

    implemented mail verification sending

--- src/controller/User.js
+++ src/controller/User.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt")
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 const User = require("../model/User")
 const saltRounds = 10
+
+dotenv.config()
 
 let createUser = async (req, res) => {
     try {
@@ -16,11 +20,51 @@ let createUser = async (req, res) => {
             phoneNumber,
         })
 
+        //function for sending mail
+        sendVerficationEmail(newUser)
+
         await newUser.save()
         res.status(201).json(newUser)
     } catch (error) {
         console.error(error)
         res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "raheem31052001@gmail.com",
+        pass: process.env.MAIL_PASSWORD
+    }
+})
+
+let sendVerficationEmail = async (user) => {
+    const verificationLink = `http://localhost:3000/user/verifyUser?token=${user.verificationToken}`
+
+    const mailOptions = {
+        from: "Shopping App <raheem31052001@gmail.com>",
+        to: user.email,
+        subject: "Verify your email address",
+        text: `
+        Hello ${user.name},
+        
+        Please click on the link below to verify your email address:
+        
+        ${verificationLink}
+        
+        If you did not create an account, please disregard this email.
+        
+        Thank You,
+        
+        The Shopping App Team
+        `
+    }
+    try {
+        transporter.sendMail(mailOptions)
+        console.log("verfication mail sent")
+    } catch (error) {
+        console.error(error)
     }
 }
 
